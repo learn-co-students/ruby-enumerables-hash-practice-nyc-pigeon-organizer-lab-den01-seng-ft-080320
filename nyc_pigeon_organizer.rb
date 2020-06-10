dacbbf7ad790e5774bed2402e9bd60cd597a25d8
Thomas Bachorz <tjbachorz@gmail.com>
2020-06-10 04:55:42 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,60 @@
+require 'pry'
+
+pigeon_data = {
+  :color => {
+    :purple => ["Theo", "Peter Jr.", "Lucky"],
+    :grey => ["Theo", "Peter Jr.", "Ms. K"],
+    :white => ["Queenie", "Andrew", "Ms. K", "Alex"],
+    :brown => ["Queenie", "Alex"]
+  },
+  :gender => {
+    :male => ["Alex", "Theo", "Peter Jr.", "Andrew", "Lucky"],
+    :female => ["Queenie", "Ms. K"]
+  },
+  :lives => {
+    "Subway" => ["Theo", "Queenie"],
+    "Central Park" => ["Alex", "Ms. K", "Lucky"],
+    "Library" => ["Peter Jr."],
+    "City Hall" => ["Andrew"]
+  }
+}
+
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  pigeon_names = []
+  data.each do |k, v|
+    data[k].each do |k2, v2|
+      pigeon_names << data[k][k2]
+    end
+  end
+  pigeon_names = pigeon_names.flatten.uniq
+  pigeon_list = {}
+  pigeon_names.each do |name|
+    pigeon_list[name] = {}
+    data.each do |k, v|
+      pigeon_list[name][k] = [] 
+    end
+  end
+  
+  pigeon_list
 end
+
+
+'''def nyc_pigeon_organizer(data)
+  pigeon_names = []
+  data.each do |k, v|
+    data[k].each do |k2, v2|
+      pigeon_names << data[k][k2]
+    end
+  end
+  pigeon_names = pigeon_names.flatten.uniq
+  pigeon_list = {}
+  pigeon_names.each do |name|
+    data.each do |k, v|
+      pigeon_list[name] = {k => []}
+      #binding.pry
+    end
+  end
+  pigeon_list
+end'''
+
+puts nyc_pigeon_organizer(pigeon_data)
